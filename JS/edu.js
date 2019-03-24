bef347b1c615daa2f76f66abad38265182d9b6b8
joeveloper <bebosir@gmail.com>
2019-03-24 15:33:13 +0100

    Edustripe V2.0 Updates

---
+++ JS/edu.js
@@ -0,0 +1,49 @@
+/*
+window.onscroll = () => {
+  scrollFunction();
+};
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("navbar").style.height = "70px";
+    document.getElementById("logo").style.paddingBottom = "0";
+    document.getElementById("logo").style.marginBottom = "-20px";
+    document.getElementById("navbar-right").style.fontSize = "15px";
+    document.getElementById("navbar-right").style.paddingTop = "5px";
+  } else {
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar-right").style.fontSize = "18px";
+  }
+};
+
+const anchorContainer = document.getElementById("navbar-right");
+let bookMarks = anchorContainer.getElementsByClassName("bookmark");
+
+for (let i = 0; i < bookMarks.length; i++) {
+  bookMarks[i].addEventListener("click", () => {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+*/
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+const scrollFunction = () => {
+  var row1 = document.getElementById("row1");
+  var row2 = document.getElementById("row2");
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    row1.style.width = "70%";
+    row2.style.width = "70%";
+  } else {
+    row1.style.width = "50%";
+    row2.style.width = "50%";
+  }
+};
